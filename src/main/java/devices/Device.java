7c95666e515c5d6149fc96c0534b1488a5b4db40
Eric <elymberopoulos@luc.edu>
2019-02-14 23:20:41 -0600

    datagram requests have dynamically determined addresses and ports now in the Datagram Connector class. also some slight refactoring.

--- src/main/java/devices/Device.java
+++ src/main/java/devices/Device.java
@@ -1,5 +1,8 @@
 package devices;
+
+import datagramConnect.DatagramConnector;
 import timer.Timer;
+
 import java.util.Date;
 import java.util.Scanner;
 
@@ -7,42 +10,49 @@ public class Device implements IDevice {
     private String deviceName;
     private boolean powerSwitch;
     private Timer timer;
+    private DatagramConnector datagramConnector;
+    private Thread ping;
 
-    public Device(){
+    public Device() {
         this.deviceName = null;
         this.powerSwitch = false;
-        this.timer = new Timer(0 , this);
-    }
-    public Timer getTimer(){
-        return timer;
+        this.timer = new Timer(0, this);
+        this.datagramConnector = new DatagramConnector(this);
     }
 
-    public boolean checkTimer(){
-        if(this.getTimer().isRunning()){
-            return true;
-        }
-        return false;
+    public DatagramConnector getDatagramConnector() {
+        return datagramConnector;
+    }
+
+    public Timer getTimer() {
+        return timer;
     }
 
     public String getDeviceName() {
         return deviceName;
     }
 
-    public void setDeviceName(String deviceName) {
-        this.deviceName = deviceName;
-    }
-
-    public void setPowerSwitch() {
-        if(this.powerSwitch == true){
-            this.powerSwitch = false;
-        }
-        else{
-            this.powerSwitch = true;
-        }
-    }
-
     public boolean isDeviceOn() {
         return powerSwitch;
     }
 
+    public void setDeviceName(String deviceName) {
+        this.deviceName = deviceName;
+    }
+
+
+    public boolean checkTimer() {
+        if (this.getTimer().isRunning()) {
+            return true;
+        }
+        return false;
+    }
+
+    public void setPowerSwitch() {
+        if (this.powerSwitch == true) {
+            this.powerSwitch = false;
+        } else {
+            this.powerSwitch = true;
+        }
+    }
 }
